9b48dc0efa7b5045f8d8cc371d6c22bc7d6f3e61
Thiago Bignotto <bignotto@gmail.com>
2024-12-30 12:07:16 -0300

    :floppy_disk: save commit

--- src/components/ProviderCard/styles.ts
+++ src/components/ProviderCard/styles.ts
@@ -1,4 +1,15 @@
 import styled from "styled-components/native";
 
 export const Container = styled.View`
+  flex-direction: row;
+`
+
+export const ProviderImage = styled.Image`
+  height: 100px;
+  width: 100px;
+  border-radius: 16px;
+`
+
+export const TextContainer = styled.View`
+  margin-left: 16px;
 `
